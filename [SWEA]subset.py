453a6b46b5ae2db13a554e1fd16b5c8ebc2d0576
jungbin97 <ljb5054@gmail.com>
2023-01-18 20:23:14 +0900

    [SWEA]subset 주석 수정

--- [SWEA]subset.py
+++ [SWEA]subset.py
@@ -2,8 +2,8 @@
 arr = [3,6,7,1,5,4]
 n=len(arr)
 
-for i in range(1<<n):
+for i in range(1<<n):    # [000000] ~ [111111]
     for j in range(n):
-        if i&(1<<j):
+        if i&(1<<j):    # [1] [10] ~ [100000] 해당자리 비트 넣을지 확인
             print(arr[j], end=",")
     print()
