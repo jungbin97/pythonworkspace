93c6450f06193ea0fc90bacfbee3354225a5d826
jungbin97 <ljb5054@gmail.com>
2023-02-15 07:07:09 +0900

    주석 추가

--- [SWEA]/[SWEA-5097]회전.py
+++ [SWEA]/[SWEA-5097]회전.py
@@ -1,10 +1,8 @@
 # ------------------- 나눗셈으로 해결 ------------------------
+# 나눗셈은 원형큐의 순환을 논리적으로 표현하기 위해 사용한 것
 for test_case in range(1, int(input())+1):
     N, M = map(int, input().split())
-    arr = list(map(int, input().splt()))
+    arr = list(map(int, input().split()))
 
 
-    print("#{} {}".format(test_case, arr[M % len(arr)])) # 어쩌면 원형큐랑 비슷할지도?
-
-# ------------------- 나눗셈으로 해결 ------------------------
-# queue로 해결해보기
+    print("#{} {}".format(test_case, arr[M % len(arr)])) 
