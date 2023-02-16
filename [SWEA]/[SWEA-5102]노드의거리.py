815c54220b6781b5cb59bde0447542fbf5dc4919
jungbin97 <ljb5054@gmail.com>
2023-02-16 09:04:44 +0900

    [SWEA-5102]노드의 거리 (queue, bfs)

---
+++ [SWEA]/[SWEA-5102]노드의거리.py
@@ -0,0 +1,34 @@
+# 가중치 없는 그래프에서(간선의 가중치가 1인) 최단거리 찾기 => BFS로 구현
+from collections import deque
+
+def bfs(S, G):
+    queue.append(S)     # 큐에 시작점 추가
+    visited[S] = True   # 방문처리
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:  # 인접노드 체크
+            if not visited[i]:
+                visited[i] = True # 방문처리
+                distance[i] = distance[v] + 1
+                queue.append(i)
+                if i == G:  # 도착노드
+                    return distance[i]
+
+for test_case in range(1, int(input())+1):
+    V, E = map(int, input().split())    # V:노드 개수 E:간선 개수
+    graph = [[] for _ in range(V+1)]      # 그래프를 저장할 2차원 리스트
+    visited = [False] * (V + 1)     
+    distance = [0] * (V + 1)
+    queue = deque()
+    
+    # 그래프 설정(양방향 그래프)
+    for i in range(E):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    # S: 출발노드 G: 도착노드
+    S,G = map(int, input().split())
+
+    print("#{} {}".format(test_case, bfs(S, G)))
+# ------ distance 리스트 없이 노드를 기록할 수 없을까? -----------
