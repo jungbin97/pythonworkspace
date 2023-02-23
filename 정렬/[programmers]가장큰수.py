801420385e26bb89c18075d06ffe711a61288099
jungbin97 <ljb5054@gmail.com>
2023-02-23 18:43:54 +0900

    [programmers]가장큰수 (정렬)

---
+++ 정렬/[programmers]가장큰수.py
@@ -0,0 +1,32 @@
+# ---------------------------------------------------
+#  순열을 위해 완전 탐색으로 해결 해보기
+# ---------------------------------------------------
+from
+def solution(numbers):
+    answer = []
+    
+
+
+
+
+
+
+
+
+
+# ---------------------------------------------------
+# def solution(numbers):
+#     input_list = list(map(str, numbers))
+
+#     input_list.sort(key = lambda x:x*3, reverse=True)
+    
+#     answer = ''
+#     answer = answer.join(input_list)
+#     return str(int(answer))
+
+# numbers = [3, 30, 34, 5, 9]
+# print(solution(numbers))
+# ------------ map을 list로 변환해야함.-------------
+#  list타입으로 변환하고 변수 저장해야 리스트를 다룰수 있음
+#  타입변환없을시 class "map"이 됨
+# -------------------------------------------------
