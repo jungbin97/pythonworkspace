1f34fe23451143a4dcc5e77d7cffcca8402cca2b
jungbin97 <ljb5054@gmail.com>
2023-02-23 22:26:52 +0900

    cmp_to_key 원리를 모르겠음

--- 정렬/[programmers]가장큰수.py
+++ 정렬/[programmers]가장큰수.py
@@ -1,6 +1,7 @@
 # ---------------------------------------------------
 #  순열을 위해 완전 탐색으로 해결 해보기
 # ---------------------------------------------------
+
 # from itertools import permutations
 # def solution(numbers):
 #     answer = []
@@ -18,18 +19,41 @@
 # print(solution(numbers))
 
 # ---------------------------------------------------
-def solution(numbers):
-    input_list = list(map(str, numbers))
 
-    input_list.sort(key = lambda x:x*3, reverse=True)
+# def solution(numbers):
+#     input_list = list(map(str, numbers))
+
+#     input_list.sort(key = lambda x:x*3, reverse=True)
     
-    answer = ''
-    answer = answer.join(input_list)
-    return str(int(answer))
+#     answer = ''
+#     answer = answer.join(input_list)
+#     return str(int(answer))
 
-numbers = [3, 30, 34, 5, 9]
-print(solution(numbers))
+# numbers = [3, 30, 34, 5, 9]
+# print(solution(numbers))
+
 # ------------ map을 list로 변환해야함.-------------
 #  list타입으로 변환하고 변수 저장해야 리스트를 다룰수 있음
 #  타입변환없을시 class "map"이 됨
 # ------------------------------------------------- 
+
+
+# ------------------------------------------------- 
+#  다른사람 풀이
+#   cmp_to_key 사용하는 방법?? => 내부 동작원리를 모르겠음
+# ------------------------------------------------- 
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
+
+numbers = [3, 30, 34, 5, 9]
+print(solution(numbers))
