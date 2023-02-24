8421be34eb70227c43451b3689506e33cc5a19d6
jungbin97 <ljb5054@gmail.com>
2023-02-24 15:05:31 +0900

    [프로그래머스]h-index (정렬)

---
+++ 정렬/[programmers]h-index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    answer = 0
+    citations.sort()    # 오름차순 정렬
+    h2 = []
+
+    for i in range(len(citations)):
+        h2.append(len(citations) - i)
+        if citations[i] == h2[i]:
+            answer = h2[i]
+            return answer
+    return answer
+
+print(solution([3, 0, 6, 1, 5]))
+# --------------- testcase 1개 통과 -----------
