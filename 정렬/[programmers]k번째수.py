ca3f33da194edd245fa2e6ac0ffd1e174e233901
jungbin97 <ljb5054@gmail.com>
2023-03-25 00:58:05 +0900

    최적화 코드(람다 예시)

--- 정렬/[programmers]k번째수.py
+++ 정렬/[programmers]k번째수.py
@@ -4,11 +4,20 @@
 #  2. 정렬 후, k 번째 수 추출
 #  3. 추출한 요소 리스트에 추가해줌
 # -------------------------------------------------------------------------
-def solution(array, commands):
-    answer = []
-    for i in range(len(commands)):
-        tmp = sorted(array[commands[i][0]-1:commands[i][1]])
-        answer.append(tmp[commands[i][2]-1])
-    return answer
+# def solution(array, commands):
+#     answer = []
+#     for i in range(len(commands)):
+#         tmp = sorted(array[commands[i][0]-1:commands[i][1]])
+#         answer.append(tmp[commands[i][2]-1])
+#     return answer
 
+# print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
+# -------------------------------------------------------------------------
+#   최적화 코드(람다 사용)
+# -------------------------------------------------------------------------
+
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+    
 print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
