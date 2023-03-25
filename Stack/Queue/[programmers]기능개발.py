9f3d8ef0d35fcb97333430690feada3e61c7def2
jungbin97 <ljb5054@gmail.com>
2023-03-25 18:28:28 +0900

    작업일 처리 개선(Math.ceil) -> 나머지가 있으면 무조건 올림처리

--- Stack/Queue/[programmers]기능개발.py
+++ Stack/Queue/[programmers]기능개발.py
@@ -5,7 +5,46 @@
 #  3. now보다 큰 작업일을 만날경우 now값 갱신
 #  4. 2, 3 과정 반복
 # ----------------------------------------------------------------------------
+
+# from collections import deque
+
+# def solution(progresses, speeds):
+#     answer = []
+#     q_day = deque()
+
+#     # 작업일 처리
+#     for i in range(len(progresses)):
+        
+#         day = (100 - progresses[i])//speeds[i]
+
+#         # 나머지가 있을 경우 날짜 한번 추가
+#         if (100 - progresses[i])%speeds[i]:     
+#             day += 1
+#         q_day.append(day)
+
+#     # q가 빌떄까지 반복    deque peek값 확인
+#     now = q_day[0]
+#     count = 0
+#     while q_day:
+#         if now >= q_day[0]:
+#             q_day.popleft()
+#             count += 1
+#             if len(q_day) == 0:
+#                 answer.append(count)
+#         else:
+#             now = q_day[0]        # now 값 갱신
+#             answer.append(count)
+#             count = 0
+
+#     return answer
+
+# print(solution([95, 90, 99, 99, 80, 99], [1,1,1,1,1,1]))
+
+# ----------------------------------------------------------------------------
+# 작업일 처리 개선(Math.ceil 사용) -> 나머지가 있으면 무조건 올림처리
+# ----------------------------------------------------------------------------
 from collections import deque
+import math
 
 def solution(progresses, speeds):
     answer = []
@@ -13,12 +52,7 @@ def solution(progresses, speeds):
 
     # 작업일 처리
     for i in range(len(progresses)):
-        
-        day = (100 - progresses[i])//speeds[i]
-
-        # 나머지가 있을 경우 날짜 한번 추가
-        if (100 - progresses[i])%speeds[i]:     
-            day += 1
+        day = math.ceil((100 - progresses[i])/speeds[i])
         q_day.append(day)
 
     # q가 빌떄까지 반복    deque peek값 확인
