47b61ea5e0f334e77fa4e748c2c1b58d2621e421
jungbin97 <ljb5054@gmail.com>
2023-03-30 22:26:47 +0900

    완전 탐색 소스 개선

--- 탐색/[programmers]피로도.py
+++ 탐색/[programmers]피로도.py
@@ -25,27 +25,47 @@
 # ----------------------------------------------------------------------------
 # DFS 백트래킹 사용
 # ----------------------------------------------------------------------------
-answer = 0
+# answer = 0
 
-def DFS(k, cnt, dungeons, check):
-    global answer
-    answer = max(answer, cnt)
-    for i in range(len(dungeons)):
-        if check[i] == 0 and k >= dungeons[i][0]:   # 방문하지 않은 던전, 현재피로도가 현재 방문하기위한 최소피로도 보다 크면
-            check[i] = 1
-            # 이전 노드 다시 back 할때, check 값 0으로 돌림
-            # 현재 피로도의 수도 해당 노드를 방문하기 전으로 돌림
-            DFS(k-dungeons[i][1], cnt+1, dungeons, check)
-            check[i] = 0
+# def DFS(k, cnt, dungeons, check):
+#     global answer
+#     answer = max(answer, cnt)
+#     for i in range(len(dungeons)):
+#         if check[i] == 0 and k >= dungeons[i][0]:   # 방문하지 않은 던전, 현재피로도가 현재 방문하기위한 최소피로도 보다 크면
+#             check[i] = 1
+#             # 이전 노드 다시 back 할때, check 값 0으로 돌림
+#             # 현재 피로도의 수도 해당 노드를 방문하기 전으로 돌림
+#             DFS(k-dungeons[i][1], cnt+1, dungeons, check)
+#             check[i] = 0
 
+
+# def solution(k, dungeons):
+#     global answer
+#     check = [0]*len(dungeons)       # 방문 여부 체크
+
+#     # cnt : 탐험한 던전 개수, k : 남은 피로도
+#     DFS(k, 0, dungeons, check)      # 방문한 던전의 개수를 0으로 초기화
+
+#     return answer
+
+# print(solution(80, [[80,20],[50,40],[30,10]]))
+# ----------------------------------------------------------------------------
+# 완전탐색 개선(비교 횟수 개선)
+# ----------------------------------------------------------------------------
+from itertools import permutations
 
 def solution(k, dungeons):
-    global answer
-    check = [0]*len(dungeons)       # 방문 여부 체크
-
-    # cnt : 탐험한 던전 개수, k : 남은 피로도
-    DFS(k, 0, dungeons, check)      # 방문한 던전의 개수를 0으로 초기화
+    answer = 0
+    l = len(dungeons)
+    for i in permutations(range(l)):
+        current_k = k
+        cnt = 0
+        for t in i:
+            require, consum = dungeons[t]
+            if current_k >= require:     # 최소피로도 보다 크거나 같으면
+                cnt += 1                 # 던전 방문 카운트 +1
+                current_k -= consum      # 방문한 던전 소모피로도 만큼 감소
+        answer = max(answer, cnt)
 
     return answer
-
 print(solution(80, [[80,20],[50,40],[30,10]]))
