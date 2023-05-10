957e6450c264ddc948994b7c3e2500cc7c362232
jungbin97 <ljb5054@gmail.com>
2023-05-10 14:54:38 +0900

    코드 단축

--- [SWEA]/[SWEA-5431]민석이의과제체크하기.py
+++ [SWEA]/[SWEA-5431]민석이의과제체크하기.py
@@ -5,24 +5,41 @@
 #  시간 복잡도 : O(n)
 # ----------------------------------------------------------------------------
 
+# for test_case in range(1, int(input())+1):
+#     # 수강생 수 : N, 과제를 제출한 사람 수 : K
+#     N, K = map(int, input().split())
+
+#     # 전체 학생 리스트
+#     totalList = [False for _ in range(1,N+1)]
+    
+#     # 과제를 제출한 번호 리스트
+#     submitList = list(map(int, input().split()))
+    
+#     for i in submitList:
+#         if totalList[i-1] == False:
+#             totalList[i-1] = True
+    
+#     nonSumitList = []
+#     for i, j in enumerate(totalList):
+#         if j == False:
+#             nonSumitList.append(i+1)
+    
+#     result = " ".join(map(str, nonSumitList))
+#     print("#%d %s" %(test_case, result))
+
+# ----------------------------------------------------------------------------
+#  시간 복잡도 O(N*K)
+# ----------------------------------------------------------------------------
 for test_case in range(1, int(input())+1):
     # 수강생 수 : N, 과제를 제출한 사람 수 : K
     N, K = map(int, input().split())
 
-    # 전체 학생 리스트
-    totalList = [False for _ in range(1,N+1)]
-    
-    # 과제를 제출한 번호 리스트
+    # 과제를 제출한 학생 번호
     submitList = list(map(int, input().split()))
-    
-    for i in submitList:
-        if totalList[i-1] == False:
-            totalList[i-1] = True
-    
-    nonSumitList = []
-    for i, j in enumerate(totalList):
-        if j == False:
-            nonSumitList.append(i+1)
-    
-    result = " ".join(map(str, nonSumitList))
-    print("#%d %s" %(test_case, result))
+    result = []
+
+    for i in range(1, N+1):
+        if i not in submitList:
+            result.append(i)
+
+    print("#{} {}".format(test_case, " ".join(map(str, result))))
