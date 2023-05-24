d4e312344c2504da58c05d9709509c4b9558d299
jungbin97 <ljb5054@gmail.com>
2023-05-24 19:53:46 +0900

    [programmers]전화번호 목록(hash, python)

--- hash/[programmers]전화번호목록.py
+++ hash/[programmers]전화번호목록.py
@@ -21,17 +21,37 @@
 # 비교해줄 필요가 없다.
 # 2. 또한 양방향으로 6 -> 12가 같은지 확인 할 필요가 없다.
 # ----------------------------------------------------------------------------
-def solution(phone_book):
-    # 전화번호 sorting
-    phone_book.sort()
+# def solution(phone_book):
+#     # 전화번호 sorting
+#     phone_book.sort()
 
-    # sorting 한 전화번호부를 2개씩 확인해서 접두어인지 확인
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False
+#     # sorting 한 전화번호부를 2개씩 확인해서 접두어인지 확인
+#     for i in range(len(phone_book)-1):
+#         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+#             return False
+#     return True
+
+# print(solution(["6789", "68"]))
+# ----------------------------------------------------------------------------
+# 해시 사용
+# value == 1 숫자가 1개 존자한다는 의미
+# 각 문자의 서브스트링을 뽑아 문자열 해쉬값이 같은 것이 hashMap에 존재한다면 false를 반환
+# ----------------------------------------------------------------------------
+def solution(phone_book):
+    # Hash map을 만든다
+    hashMap = {}
+    for i in phone_book:
+        hashMap[i] = 1
+
+    # 접두어가 Hash map에 존재하는 찾는다.
+    for nums in phone_book:
+        arr = ""
+        for num in nums:
+            arr += num
+
+            # 본인 자체일 경우는 제외
+            if arr in hashMap and arr != nums:
+                return False
     return True
 
-print(solution(["6789", "68"]))
-# ----------------------------------------------------------------------------
-# 
-# ----------------------------------------------------------------------------
+print(solution(["119", "97674223", "1195524421"]))
