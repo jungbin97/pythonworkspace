84b891591f690ce2104d14bf64ba4418eb1fc57e
jungbin97 <ljb5054@gmail.com>
2023-05-24 18:14:57 +0900

    [programmers]전화번호목록(정렬 ,python)

--- hash/[programmers]전화번호목록.py
+++ hash/[programmers]전화번호목록.py
@@ -3,14 +3,35 @@
 #  1. startswith를 사용해 문자열이 서로 접두어인지 확인한다.
 # ["6", "12", "6789"] 일때 6 -> 1, 12 -> 6 
 # ----------------------------------------------------------------------------
+# def solution(phone_book):
+#     for i in range(len(phone_book)):
+#         for j in range(i+1, len(phone_book)):
+#             if phone_book[i].startswith(phone_book[j]):
+#                 return False
+#             if phone_book[j].startswith(phone_book[i]):
+#                 return False
+#     return True
+
+
+# print(solution(["6", "12", "6789"]))
+# ----------------------------------------------------------------------------
+# 정렬을 통해 비교한 연산을 줄여준다.
+# ["12", "6", "6789"]이라면
+# 1. 정렬을 오름차순으로 해주었으므로 12 -> 6 이다르면 12와 6이후에 것들은 어짜피 다르기떄문에
+# 비교해줄 필요가 없다.
+# 2. 또한 양방향으로 6 -> 12가 같은지 확인 할 필요가 없다.
+# ----------------------------------------------------------------------------
 def solution(phone_book):
-    for i in range(len(phone_book)):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[i].startswith(phone_book[j]):
-                return False
-            if phone_book[j].startswith(phone_book[i]):
-                return False
+    # 전화번호 sorting
+    phone_book.sort()
+
+    # sorting 한 전화번호부를 2개씩 확인해서 접두어인지 확인
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
     return True
 
-
-print(solution(["6", "12", "6789"]))
+print(solution(["6789", "68"]))
+# ----------------------------------------------------------------------------
+# 
+# ----------------------------------------------------------------------------
