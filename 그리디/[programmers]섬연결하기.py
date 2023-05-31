5a9113ddd2741857fdb3283f4195225669e0cac0
jungbin97 <ljb5054@gmail.com>
2023-05-31 20:32:10 +0900

    [programmers] 섬연결하기(kruskal)

---
+++ 그리디/[programmers]섬연결하기.py
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------
+# [programmers] 섬연결하기 그리디(kruskal)
+# 크루스칼 알고리즘 정석 풀이
+# ----------------------------------------------------------------------------
+def getParent(parent, x):
+    if parent[x] == x:
+        return x
+    return getParent(parent, parent[x])
+
+def unionParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def compareParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+    if a == b:
+        return True
+    else:
+        return False
+    
+
+def solution(n, costs):
+    answer = 0
+    count = 0
+
+    parent = [0] * n    # 노드 개수 만큼 부모노드 테이블 초기화
+    for i in range(n):
+        parent[i] = i
+
+    # 간선을 비용순으로 정렬
+    costs.sort(key = lambda x: x[2])
+
+    for x in costs:
+        # 사이클 여부 체크
+        if not compareParent(parent, x[0], x[1]):
+            answer += x[2]
+            unionParent(parent, x[0], x[1])
+            count += 1
+        
+        # 크루스칼 알고리즘은 연결된 간선의 개수가 노드의 개수 -1이기 떄문에
+        if count == n-1:
+            break
+
+    return answer
+
+print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
