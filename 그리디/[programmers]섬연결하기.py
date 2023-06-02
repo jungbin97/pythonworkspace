798fac99c0e2568d5edb984a619e9d10530a6d06
jungbin97 <ljb5054@gmail.com>
2023-06-02 14:54:17 +0900

    최적화

--- 그리디/[programmers]섬연결하기.py
+++ 그리디/[programmers]섬연결하기.py
@@ -49,3 +49,43 @@ def solution(n, costs):
     return answer
 
 print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
+
+# ----------------------------------------------------------------------------
+# comapreParent 함수 제거 최적화
+# ----------------------------------------------------------------------------
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def solution(n, costs):
+    answer = 0
+    # 크루스칼 알고리즘 조건 만족하는지 확인 변수 count
+    count = 0
+
+    parent = [0]*n # 노드 개수만큼 부모노드 테이블 초기화
+    for i in range(n):
+        parent[i] = i
+
+    # 간선을 비용 순서로 정렬
+    costs.sort(key = lambda x:x[2])
+
+    for x in costs:
+        # 사이클 여부 확인
+        if find_parent(parent, x[0]) != find_parent(parent, x[1]):
+            union_parent(parent, x[0], x[1])
+            answer += x[2]
+            count += 1
+
+        # 크루스칼 알고리즘 연결된 간선의 개수는 노드 개수 -1
+        if count == n - 1:
+            break
+    return answer
