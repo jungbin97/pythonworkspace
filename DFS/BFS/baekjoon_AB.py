bd6570cdcb576d3300170b4d1eedb3c734dd2a6e
jungbin97 <ljb5054@gmail.com>
2023-09-20 12:59:14 +0900

    [baekjoon_16953] A->B

---
+++ DFS/BFS/baekjoon_AB.py
@@ -0,0 +1,25 @@
+# ----------------------------------------------------------------------------
+# [baekjoon_16953] A -> B 
+# ----------------------------------------------------------------------------
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+def solution(a,b):
+    q = deque([(a, 1)])
+
+    while q:
+        a, cnt = q.popleft()
+        if a==b:
+            print(cnt)
+            return
+        
+        if a * 2 <= b:
+            q.append((a*2, cnt+1))
+        if a * 10 + 1 <= b:
+            q.append((a*10+1, cnt+1))
+    print(-1)
+
+solution(a,b)
